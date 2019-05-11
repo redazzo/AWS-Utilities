62ecb001b6dc487debaea24eaa06a208f486feb2
redazzo <gerhardhaak@gmail.com>
2019-05-11 19:31:45 +1200

    Updated to generate jar instead of POM

---
+++ src/test/java/nz/redazzo/utility/aws/AWSLambdaAuthorisationHeaderV4Test.java
@@ -0,0 +1,20 @@
+package nz.redazzo.utility.aws;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AWSLambdaAuthorisationHeaderV4Test {
+
+    @Test
+    public void setRegion() {
+    }
+
+    @Test
+    public void setCredentials() {
+    }
+
+    @Test
+    public void signHTTPHeaderRequest() {
+    }
+}
